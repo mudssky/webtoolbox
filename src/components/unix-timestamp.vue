b263a5be17beec90e0a4785b585c462d93d76e57
mudssky <mudssky@gmail.com>
2020-12-28 17:58:03 +0800

    init navbar component

---
+++ src/components/unix-timestamp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-row>
+      <el-input v-model="currentTimeStamp" :disabled="true"> </el-input>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  name: "unix-timestamp",
+  data() {
+    return {
+      currentTimeStamp: function () {
+        const date = new Date();
+        return date.toTimeString();
+      },
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
